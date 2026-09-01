6e478c2aa07cf5be25ddcd59fe4ddc0510af65e9
Aaron <aaronbaron@hotmail.co.uk>
2026-09-01 10:47:39 +0100

    variable for timerange and setting timezone

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+locals {
+  time_range_relative   = "${var.default_time_range_hours}h"
+  time_range_display    = "Past ${var.default_time_range_hours} hour${var.default_time_range_hours == 1 ? "" : "s"}"
+  timezone_display_name = var.timezone == "local" ? "Local Time" : "UTC Time"
+}
+
 resource "azurerm_portal_dashboard" "dashboard" {
   name                = var.dashboard_name
   resource_group_name = var.resource_group_name
@@ -8,5 +14,10 @@ resource "azurerm_portal_dashboard" "dashboard" {
     dashboard_parts = jsonencode({
       for idx, part in var.dashboard_parts : tostring(idx) => jsondecode(part)
     })
+    default_time_range_hours = var.default_time_range_hours
+    time_range_relative      = local.time_range_relative
+    time_range_display       = local.time_range_display
+    timezone                 = var.timezone
+    timezone_display_name    = local.timezone_display_name
   })
 }
